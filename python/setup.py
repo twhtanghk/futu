8b5acb0602a9cd24cf298e545419a9e9c31bf7f4
root <twhtanghk@gmail.com>
2022-09-16 06:16:30 +0000

    add subscribe module

---
+++ python/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="python",
+    version="0.1.0",
+    description="futu quote subscription",
+    license="MIT",
+    author="twhtanghk@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
