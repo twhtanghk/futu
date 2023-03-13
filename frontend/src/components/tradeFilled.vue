8f19a11f7a9d1ac54ab8033d86b333313b39e5c2
root <twhtanghk@gmail.com>
2023-03-13 23:31:46 +0800

    add orderStatus field, update display format of qty and price

--- frontend/src/components/tradeFilled.vue
+++ frontend/src/components/tradeFilled.vue
@@ -2,8 +2,17 @@
   <v-data-table :sort-by='sortBy' :headers='headers' :items='trade' :items-per-page='-1' density='compact' fixed-header='true' height='100%'>
     <template v-slot:item.trdSide='{ item }'>
       <v-chip :color="item.raw.trdSide == 2 ? 'green' : 'red'">
-        {{ item.raw.trdSide == 2 ? 'Sell' : 'Buy'}}
+        {{trdSide(item.raw.trdSide)}}
       </v-chip>
+    </template>
+    <template v-slot:item.orderStatus='{ item }'>
+      {{orderStatus(item.raw.orderStatus)}}
+    </template>
+    <template v-slot:item.qty='{ item }'>
+      {{item.raw.fillQty}} / {{item.raw.qty}}
+    </template>
+    <template v-slot:item.price='{ item }'>
+      {{item.raw.fillAvgPrice.toFixed(2)}} / {{item.raw.price.toFixed(2)}}
     </template>
     <template v-slot:item.createTimestamp='{ item }'>
       {{new Date(1000 * item.raw.createTimestamp).toLocaleString()}}
@@ -27,6 +36,7 @@ export default
     sortBy: [{key: 'updateTimestamp', order: 'desc'}]
     headers: [
       {title: 'Action', key: 'trdSide'}
+      {title: 'Status', key: 'orderStatus'}
       {title: 'Code', key: 'code'}
       {title: 'Name', key: 'name'}
       {title: 'Qty', key: 'qty'}
@@ -44,6 +54,29 @@ export default
           .unix last.updateTimestamp
           .format 'YYYY-MM-DD HH:mm:ss'
       @trade = @trade.concat await trade.list {endTime}
+    trdSide: (i) ->
+      ['Unknown', 'Buy', 'Sell', 'SellShort', 'Buyback'][i]
+    orderStatus: (i) ->
+      map = {
+        '-1': 'Unknown'
+        0: 'Unsubmitted'
+        1: 'WaitingSubmit'
+        2: 'Submitting'
+        3: 'SubmitFailed'
+        4: 'Timeout'
+        5: 'Submitted'
+        10: 'Filled_Part'
+        11: 'Filled_All'
+        12: 'Cancelling_Part'
+        13: 'Cancelling_All'
+        14: 'Cancelled_Part'
+        15: 'Cancelled_All'
+        21: 'Failed'
+        22: 'Disabled'
+        23: 'Deleted'
+        24: 'FillCancelled'
+      }
+      map[i]
   mounted: ->
     @ws = (await ws)
       .on 'message', (msg) =>
@@ -52,6 +85,7 @@ export default
         if topic == 'trdFilled'
           @trade.push _.pick orderFill, [
             'trdSide'
+            'orderStatus'
             'code'
             'name'
             'qty'
